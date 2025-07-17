89894ba6faf2856047f880b1eaeb35d2ca74984d
zollahrp <141205140+zollahrp@users.noreply.github.com>
2025-07-17 11:06:57 +0700

    fix error while deploy

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,4 +1,5 @@
-// src/app/loading.jsx
+import Loader from '@/components/ui/Loader';
+
 export default function Loading() {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-white">
